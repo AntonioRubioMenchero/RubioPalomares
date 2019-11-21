c577ff9654746ad7ae0ab34630a05c28dcb14df5
JavierPalomaresGarrido <56022626+JavierPalomaresGarrido@users.noreply.github.com>
2019-11-21 20:06:27 +0100

    Actualizacion

--- orchestrator.py
+++ orchestrator.py
@@ -17,3 +17,31 @@ class Orchestrator(TrawlNet.Orchestrator):
         print(msg)
 
         return msg
+
+
+class Server(Ice.Application):
+    def run(self, argv):
+        print('Iniciando servidor Orchestrato')
+        broker = self.communicator()
+        prx_dw = broker.stringToProxy(argv[1])
+        downloader = TrawlNet.DownloaderPrx.checkedCast(prx_dw)
+        if not downloader:
+
+            raise RuntimeError('Error')
+
+        # Creamos el orchestrator
+        server = Orchestrator(downloader)
+
+        #Añadimos adapatdor
+        adapter = broker.createObjectAdapter("OrchestratorAdapter")
+        prx_orches = adapter.add(server, broker.stringToIdentity("orc"))
+
+        print("",prx_orches)
+        adapter.activate()
+        self.shutdownOnInterrupt()
+        broker.waitForShutdown()
+
+        return 0
+
+server = Server()
+sys.exit(server.main(sys.argv))
